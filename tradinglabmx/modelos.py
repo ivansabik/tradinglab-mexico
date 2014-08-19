7c5f5a3eb8d3888f47cd3ec9ceed6415cb6df4f1
sabik <ivan.rodriguez.o@gmail.com>
2014-08-19 00:28:24 -0500

    clases comenzando

---
+++ tradinglabmx/modelos.py
@@ -0,0 +1,19 @@
+# Emisora
+class Emisora:
+    clave_bmv = ''
+    nombre = ''
+    clave_yahoo = ''
+    clave_bloomberg = ''
+    fecha_constitucion = ''
+    fecha_listado = ''
+    sector = ''
+    subsector = ''
+    ramo = ''
+    subramo = ''
+
+# Movimiento de trading
+class MovimientoTrading:
+    is_compra = ''
+    num_acciones = ''
+    emisora = ''
+    fecha = ''
---
+++ tradinglabmx/sandbox.py
@@ -0,0 +1,17 @@
+from modelos import Emisora
+
+emisora = Emisora()
+emisora.clave_bmv = 'WALMEX'
+emisora.nombre = 'Wal Mart'
+
+print emisora.clave_bmv
+print emisora.nombre
+
+from modelos import MovimientoTrading
+
+movimiento = MovimientoTrading()
+movimiento.is_compra = True
+movimiento.num_acciones = 500
+
+print movimiento.is_compra
+print movimiento.num_acciones
