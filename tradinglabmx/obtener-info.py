29c9ccf1f85faf88f9b67e8d540814a71cc389f4
sabik <ivan.rodriguez.o@gmail.com>
2014-08-19 00:44:26 -0500

    nonexistum eliminado, ya no hay en csv

--- tradinglabmx/obtener-info.py
+++ tradinglabmx/obtener-info.py
@@ -10,15 +10,14 @@ print empresas
 datos_consolidados = pd.DataFrame()
 
 for empresa in empresas:
-    if(empresa != 'NONEXISTUM.NA'):
-        datos = pd.io.data.get_data_yahoo(empresa, 
-                                         start=datetime.datetime(1990, 1, 1), 
-                                         end=datetime.datetime(2015, 1, 1))
-        datos['clave_empresa'] = empresa
-        print empresa
-        print datos.head()
-        datos_consolidados = datos_consolidados.append(datos)
-        datos.to_csv('./csv/' + empresa + '.csv')
+    datos = pd.io.data.get_data_yahoo(empresa, 
+                                     start=datetime.datetime(1990, 1, 1), 
+                                     end=datetime.datetime(2015, 1, 1))
+    datos['clave_empresa'] = empresa
+    print empresa
+    print datos.head()
+    datos_consolidados = datos_consolidados.append(datos)
+    datos.to_csv('./csv/' + empresa + '.csv')
 print datos_consolidados.head()
 datos_consolidados.to_csv('./historicos-bmv.csv')
 
